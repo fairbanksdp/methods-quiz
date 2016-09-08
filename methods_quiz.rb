b0d20786ac51b861d0227160defada5827b374d0
Daniel Patrick Fairbanks <fairbanksdp@dcsdk12.org>
2016-09-08 13:57:47 -0600

    Implement not_string

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -4,7 +4,12 @@ def has_teen?(a,b,c)
   a>12&&a<20||b>12&&b<20||c>12&&c<20
 end
 # TODO - write not_string
-
+def not_string(str)
+  if str.slice(0..2)!='not'
+    str='not'+str
+  end
+  str
+end
 # TODO - write icy_hot?
 
 # TODO - write closer_to

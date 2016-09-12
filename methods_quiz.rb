359f37db73785b5ac7081b8480cf4ffeaf4722c0
DanBaggins <danielfairbanks@comcast.net>
2016-09-11 22:03:30 -0600

    Implement vowel? and word_split

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,4 +1,6 @@
-	
+str.$stdin.get
+
+newString=Array.new	
 # TODO - write has_teen?
 def has_teen?(a,b,c)
   a>12&&a<20||b>12&&b<20||c>12&&c<20
@@ -38,18 +40,35 @@ def two_as_one?(a,b,c)
   a+b==c||a+c==b||b+c==a
 end
 # TODO - write pig_latinify
-def word_split(str)
-  letterCount=str.index(' ')
-  if letterCount!=nil
-    word_
+def word_split(str,n)
+  space?=str.index(' ')
+  if space?!=nil
+    newString(n)=[str.slice(0..space?)]
+    str.replace str.slice(spac?+1..str.length)
+    word_split(str,n+1)
+  else
+    newString[n]=[str.slice(0..space?)]
+  end
+  newString
+end
 
-def pig_latinify(str)
-  str=str.downcase.chop
-  
+def vowel?(str)
   if str.slice(0)=='a'||str.slice(0)=='e'||str.slice(0)=='i'||str.slice(0)=='o'||str.slice(0)=='u'||str.slice(0)=='y'
     return str+'way'
   end
   str.slice(1..str.length)+str.slice(0)+'ay'
-end 
+end
 
+def pig_latinify(str)
+  str=str.downcase.chomp
+  word_split(str,0)
+  n=0
+  while newString.length - n > 0
+    #if newString[n]!=nil
+      str = newString[n]
+      finStr=finStr+vowel?(str) 
+    #end
+  end
+end 
+pig_latinify(str)
 #tutnese 
